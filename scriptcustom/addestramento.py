9587a26d3e83ca11e4efc02311361f86c0f129db
Alessio Leo <a.leo88@studenti.unisa.it>
2025-09-24 19:04:19 +0200

    Update addestramento.py

--- scriptcustom/addestramento.py
+++ scriptcustom/addestramento.py
@@ -5,7 +5,7 @@ df = pd.read_csv("dataset_livelli.csv")
 X = df[['rtt_medio', 'pdr_percent']]
 y = df['livello']
 
-# Gesstione dataset
+# Gestione dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
